83b77684e9e41bd825e6845a82727d24547d9302
superfishal <97133716+superfishal@users.noreply.github.com>
2022-04-26 16:28:43 -0700

    comment routes completed

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Vote = require("./Vote");
 const Post = require("./Post");
 const User = require("./User");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
@@ -35,4 +36,19 @@ User.hasMany(Vote, {
 Post.hasMany(Vote, {
   foreignKey: "post_id",
 });
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+module.exports = { User, Post, Vote, Comment };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
+    order: [["created_at", "DESC"]],
     attributes: [
       "id",
       "post_url",
@@ -18,8 +19,16 @@ router.get("/", (req, res) => {
         "vote_count",
       ],
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
