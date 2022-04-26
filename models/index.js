dcca8998eac534af7981d86b62b5386f8b4b6ab6
superfishal <97133716+superfishal@users.noreply.github.com>
2022-04-26 16:03:46 -0700

    refractor post routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const Vote = require("./Vote");
 const Post = require("./Post");
 const User = require("./User");
 
@@ -8,4 +9,30 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
   foreignKey: "user_id",
 });
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Vote, {
+  foreignKey: "post_id",
+});
+module.exports = { User, Post, Vote };
