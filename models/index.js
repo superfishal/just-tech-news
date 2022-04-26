f9bd64de6d54d79fb7462578b4d9597b1311c8df
superfishal <97133716+superfishal@users.noreply.github.com>
2022-04-26 14:58:18 -0700

    user and post CRUD

--- models/index.js
+++ models/index.js
@@ -1,4 +1,11 @@
-const User = require("./User");
 const Post = require("./Post");
+const User = require("./User");
 
+// create associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+// turn on connection to db and server | force: true recreates the tables
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
