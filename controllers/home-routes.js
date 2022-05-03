2128292798c2e1134a4ab133ddab8d9fbdcf951b
superfishal <97133716+superfishal@users.noreply.github.com>
2022-05-03 15:49:35 -0700

    added loggedIn conditionals

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,7 +36,10 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
 
-      res.render("homepage", { posts });
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -93,7 +96,10 @@ router.get("/post/:id", (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render("single-post", { post });
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
     })
     .catch((err) => {
       console.log(err);
